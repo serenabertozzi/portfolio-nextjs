f07349f98f59d951d802c3f5b6f0d9de3e7dd535
serenabertozzi <serenabertozzi@protonmail.com>
2021-12-27 19:22:46 +0100

    Add content to index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import { Container, Box } from '@chakra-ui/react'
+import { Container, Box, Heading } from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <div>Hello</div>
+      <Box borderRadius="lg" bg="blue" p={3} mb={6} align="center">
+        Hello I&apos;m a full-stack developer based in London, UK!
+      </Box>
+      <Box display={{ md: 'flex' }} align="center">
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Serena Bertozzi
+          </Heading>
+          <p>Developer | Makers graduate | Content Creator</p>
+        </Box>
+      </Box>
     </Container>
   )
 }
